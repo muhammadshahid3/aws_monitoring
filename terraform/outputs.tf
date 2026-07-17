a3f603969050b44013278a70d1e02deace9f47f8
Muhammad Shahid <147014897+muhammadshahid3@users.noreply.github.com>
2026-07-17 22:56:31 +0430

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,21 +1,31 @@
+output "configuration_public_ip" {
+  value = aws_instance.configuration.public_ip
+}
 
-output "monitoring_server_public_ip" {
+output "configuration_private_ip" {
+  value = aws_instance.configuration.private_ip
+}
+
+output "monitoring_public_ip" {
   value = aws_instance.monitoring.public_ip
 }
-output "app_server_1_public_ip" {
-  value = aws_instance.app1.public_ip
-}
-output "app_server_2_public_ip" {
-  value = aws_instance.app2.public_ip
-}
 
-# private ip
-output "monitoring_server_private_ip" {
+output "monitoring_private_ip" {
   value = aws_instance.monitoring.private_ip
 }
-output "app_server_1_private_ip" {
-  value = aws_instance.app1.private_ip
+
+output "node1_public_ip" {
+  value = aws_instance.node1.public_ip
 }
-output "app_server_2_private_ip" {
-  value = aws_instance.app2.private_ip
+
+output "node1_private_ip" {
+  value = aws_instance.node1.private_ip
+}
+
+output "node2_public_ip" {
+  value = aws_instance.node2.public_ip
+}
+
+output "node2_private_ip" {
+  value = aws_instance.node2.private_ip
 }
